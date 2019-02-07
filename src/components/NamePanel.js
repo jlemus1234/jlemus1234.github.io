ed504bb2a3aca196409b8cc0087b40272af5a6d5
jlemus1234 <jlemus1234@gmail.com>
2019-02-06 23:26:25 -0500

    Restarted with Gatsby

---
+++ src/components/NamePanel.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+class NamePanel extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+
+
+    render(){
+        return <div></div>;
+    }
+}
+
+export default NamePanel;
