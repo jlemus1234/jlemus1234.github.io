a3397a080bf434c7b644723ab20557a9006d5d2d
jlemus1234 <jlemus1234@gmail.com>
2019-07-31 18:38:17 -0400

    updated dependencies

---
+++ src/components/ImageBanner.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+class NamePanel extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            
+        };
+
+        <div>
+            
+
+        </div>
+    }
+
+
+
+    render(){
+        return <div></div>;
+    }
+}
+
+export default NamePanel;
