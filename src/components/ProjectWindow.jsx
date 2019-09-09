b1c4ba9d78a2b94e3f0da0037c6967944b41b921
jlemus1234 <jlemus1234@gmail.com>
2019-09-08 23:48:09 -0400

    Began adding in project display components

---
+++ src/components/ProjectWindow.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class ProjectWindow extends React.Component {
+    constructor(props){
+        // required props include a name and an image/video
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render(){
+        return (
+            <div id="video">
+
+            </div>
+        )
+    }
+}
+
+export default ProjectWindow;
---
+++ src/components/ProjectsPanel.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+class ProjectsPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ProjectsPanel;
