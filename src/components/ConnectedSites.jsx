27df9bf21eea4ebb85e902bd9b5824a10196b36c
jlemus1234 <jlemus1234@gmail.com>
2019-10-26 18:16:28 -0400

    Fixed images and links in ConnectedSites prop

--- src/components/ConnectedSites.jsx
+++ src/components/ConnectedSites.jsx
@@ -6,22 +6,6 @@ import linkedinlogo from './../images/Icons/linkedin.svg'
 import emaillogo from './../images/Icons/gmail.svg'
 
 
-const styles = {
-    otherSitesStyle : {
-    display: 'flex',
-    justifyContent: 'center',
-    width: '100%',
-    height: '25px',
-    background:'gray'
-    },
-
-    logoStyle : {
-    display: 'block',
-    marginLeft: '50%'
-    }
-};
-
-
 // Make a set number of logo's / sites that can be connected
 // Make the props only the specific links to each site
 // Make props/logos visible only if the specific prop was passed in or not
@@ -39,19 +23,46 @@ class ConnectedSites extends React.Component {
 
     render() {
         return (
-            <div>
-                {/* Testing */}
-                <div>
+                <div style={styles.container}>
                     <div style={styles.otherSitesStyle} className="otherSites" id="otherSitesContainer">
-                        <a href="https://github.com/jlemus1234"><img style={styles.logoStyle} className="link" src={githublogo} alt="GitHub Logo"></img></a>
-                        <a href="https://www.linkedin.com/in/jose-lemus-404328157/"><img style={styles.logoStyle} className="link" src={linkedinlogo} alt="LinkedIn Logo"></img></a>
-                        <a href="mailto: jlemus1234@gmail.com"><img style={styles.logoStyle} className="link" src={emaillogo} alt="E-Mail Logo"></img></a>
+                        <a style={styles.link} href="https://github.com/jlemus1234"><img style={styles.logoStyle} src={githublogo} alt="GitHub Logo"></img></a>
+                        <a style={styles.link} href="https://www.linkedin.com/in/jose-lemus-404328157/"><img style={styles.logoStyle} src={linkedinlogo} alt="LinkedIn Logo"></img></a>
+                        <a style={styles.link} href="mailto: jlemus1234@gmail.com"><img style={styles.logoStyle} src={emaillogo} alt="E-Mail Logo"></img></a>
                     </div>
                 </div>
-            </div>
         )
     }
 }
+
+const styles = {
+    container: {
+        display: 'flex',
+        width: '100%',
+        background: 'gray',
+        justifyContent: 'center'
+    },
+
+    otherSitesStyle : {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '50%',
+    //height: '25px',
+    marginBottom: '0'
+    },
+
+    logoStyle : {
+    display: 'block',
+    //marginLeft: '50%',
+    height: '25px',
+    width: '25px',
+    marginBottom: '0'
+    },
+
+    link : {
+        marginBottom: '0'
+    }
+};
+
 
 
 ConnectedSites.defaultProps = {
