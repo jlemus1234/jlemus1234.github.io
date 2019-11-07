0ea92ae33882336021c5bebbcf203cac345ab754
jlemus1234 <jlemus1234@gmail.com>
2019-11-07 18:12:06 -0500

    Removed maxWidth limits in layout, made imageslideshow now the full width of the screen

--- src/components/header.js
+++ src/components/header.js
@@ -10,13 +10,13 @@ const Header = ({siteTitle, currentPage}) => (
       // Sets the color for the header
       //background: 'rgb(181, 188, 199)',
       background: 'rgb(240,240,240)',
-      marginBottom: `1.45rem`,
+      // marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
+        // margin: `0 auto`,
+        // maxWidth: 960, // this is set here, and also in the layout component
         padding: `1.45rem 1.0875rem`,
         //adding
         display: 'flex',
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -9,8 +9,8 @@ import collabImg from '../assets/SwimSense/SwimSense8.jpg'
 
 const AboutMe = () => (
     <Layout currentPage="/AboutMe">
-        <h1 style={style.title}>About Me</h1>
         <ImageSlideShow style={{ height: '400px' }} imageList={[collabImg, headshot]}> </ImageSlideShow>
+        <h1 style={style.title}>About Me</h1>
         <div style={style.text}>
             <p>
                 Hello I'm Jose Lemus. I like to tinker with computers.
